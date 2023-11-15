f9ea822481cd457a0c1e0437da2968be14d8175a
LingZhouChen <lingzhouc@gmail.com>
2023-11-15 01:24:42 -0500

    complete lab

--- server/models.py
+++ server/models.py
@@ -7,12 +7,16 @@ metadata = MetaData()
 # create the Flask SQLAlchemy extension
 db = SQLAlchemy(metadata=metadata)
 
-# define a model class by inheriting from db.Model.
-
-
+# define a model class by inheriting from db.Model
+    # Pet class declared as subclass of db.Model
 class Pet(db.Model):
+
+    # database table is named pets
     __tablename__ = 'pets'
 
+    # database table has 3 columns
+        # id column is the primary key
+        # name and species column store a string
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     species = db.Column(db.String)
